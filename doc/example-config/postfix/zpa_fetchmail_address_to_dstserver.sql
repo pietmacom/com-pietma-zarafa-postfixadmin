f64929a39165bcc31ab620b969bcb662dcb12ff2
MartiMcFly <martimcfly@pietma.com>
2016-12-10 01:16:21 +0100

    sql for postfix

---
+++ doc/example-config/postfix/zpa_fetchmail_address_to_dstserver.sql
@@ -0,0 +1,6 @@
+user = zarafapostfixadmin
+password = 
+hosts = unix:/run/mysqld/mysqld.sock
+dbname = zarafapostfixadmin
+query = SELECT dst_server FROM fetchmail WHERE mailbox = '%s' AND dst_server IS NOT NULL AND dst_server <> '' AND active = '1' ORDER BY src_user, dst_server;
+
