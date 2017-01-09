cb63090059b284a57d98fca38ffe2f79fd4e86f9
MartiMcFly <martimcfly@pietma.com>
2017-01-09 02:34:31 +0000

    smarthost administration for domains

--- webapp/config.local.php
+++ webapp/config.local.php
@@ -15,6 +15,9 @@ function domain_struct_admin_modify($struct) {
     $struct['default_aliases']['default'] = 0;
     $struct['backupmx']['display_in_form'] = 0;
     $struct['backupmx']['default'] = 0;
+    $struct['dst_server']	= pacol(   1,          1,      1,      'text', 'pAdminEdit_domain_dst_server'    , 'pAdminEdit_domain_desc_dst_server'       );
+    $struct['dst_user']		= pacol(   1,          1,      1,      'text', 'pAdminEdit_domain_dst_user'      , 'pAdminEdit_domain_desc_dst_user'         );
+    $struct['dst_password']	= pacol(   1,          1,      0,      'b64p', 'pAdminEdit_domain_dst_password'  , 'pAdminEdit_domain_desc_dst_password'     );
     return $struct;
 }
 $CONF['domain_struct_hook'] = 'domain_struct_admin_modify';
@@ -39,8 +42,8 @@ function mailbox_struct_admin_modify($struct) {
 $CONF['mailbox_struct_hook'] = 'mailbox_struct_admin_modify';
 
 function fetchmail_struct_admin_modify($struct) {
-    $struct['dst_server']    = pacol(   1,          1,      1,      'text', 'pFetchmail_field_dst_server'   , 'pFetchmail_desc_dst_server'      );
-    $struct['dst_address']    = pacol(   1,          1,      1,      'text', 'pFetchmail_field_dst_address'   , 'pFetchmail_desc_dst_address'      );    
+    $struct['dst_server']	= pacol(   1,          1,      1,      'text', 'pFetchmail_field_dst_server'    , 'pFetchmail_desc_dst_server'       );
+    $struct['dst_address']	= pacol(   1,          1,      1,      'text', 'pFetchmail_field_dst_address'   , 'pFetchmail_desc_dst_address'      );
     return $struct;
 }
 $CONF['fetchmail_struct_hook'] = 'fetchmail_struct_admin_modify';
@@ -49,6 +52,15 @@ $CONF['fetchmail_struct_hook'] = 'fetchmail_struct_admin_modify';
 function language_hook($PALANG, $language) {
     switch ($language) {
         default:
+            $PALANG['pAdminEdit_domain_dst_server'] = 'Outbound Server';
+            $PALANG['pAdminEdit_domain_desc_dst_server'] = 'SMTP Server';
+
+            $PALANG['pAdminEdit_domain_dst_user'] = 'Outbound Username';
+            $PALANG['pAdminEdit_domain_desc_dst_user'] = 'SMTP Username';
+
+            $PALANG['pAdminEdit_domain_dst_password'] = 'Outbound Password';
+            $PALANG['pAdminEdit_domain_desc_dst_password'] = 'SMTP Password';
+
             $PALANG['pCreate_mailbox_password_text'] = 'Initial password (transferred / generated when empty). Please change in Zarafa!';
 
             $PALANG['pFetchmail_field_src_server'] = 'Inbound Server';
@@ -56,7 +68,7 @@ function language_hook($PALANG, $language) {
 
             $PALANG['pFetchmail_field_dst_server'] = 'Outbound Server';
             $PALANG['pFetchmail_desc_dst_server'] = 'SMTP Server';
-
+            
             $PALANG['pFetchmail_field_dst_address'] = 'Outbound Address';
             $PALANG['pFetchmail_desc_dst_address'] = 'E-Mail Address';
     }
